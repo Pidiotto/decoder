aaae89de1d2bc441eff8cda53521d995351def7d
Pidiotto <wagner.nardes@gmail.com>
2024-01-28 17:41:49 -0300

    Finalização do script e adaptação do conteudo responsivo

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
+// Funçao para criptografar o texto
 function criptografarTexto(texto) {
-    // Lógica de criptografia
     // Substituições conforme as regras fornecidas
     const cripto = texto
         .replace(/e/g, 'enter')
@@ -9,9 +9,9 @@ function criptografarTexto(texto) {
         .replace(/u/g, 'ufat');
     return cripto;
 }
-// Função para descriptografar
+
+// Funçao para descriptografar o texto
 function descriptografarTexto(textoCriptografado) {
-    // Lógica de descriptografia
     // Substituições inversas
     const descripto = textoCriptografado
         .replace(/ufat/g, 'u')
@@ -22,24 +22,57 @@ function descriptografarTexto(textoCriptografado) {
     return descripto;
 }
 
-document.getElementById('criptografar').addEventListener('click', function () {
+function processarTexto(operacao) {
+    // Obtem o texto original do elemento de texto
     const textoOriginal = document.getElementById('texto').value;
-    const textoCriptografado = criptografarTexto(textoOriginal);
-    document.getElementById('resultado').innerText = textoCriptografado;
+
+    // executa a operação (criptografar ou descriptografar) com base no argumento
+    const resultado = (operacao === 'criptografar') ? criptografarTexto(textoOriginal) : descriptografarTexto(textoOriginal);
+
+    // exibe o resultado no elemento de resultado
+    document.getElementById('resultado').value = resultado;
+
+    esconderMsg();
+}
+
+// ouvinte botão de criptografar
+document.getElementById('criptografar').addEventListener('click', function () {
+    processarTexto('criptografar');
 });
 
+// ouvinte botão de descriptografar
 document.getElementById('descriptografar').addEventListener('click', function () {
-    const textoCriptografado = document.getElementById('texto').value;
-    const textoDescriptografado = descriptografarTexto(textoCriptografado);
-    document.getElementById('resultado').innerText = textoDescriptografado;
+    processarTexto('descriptografar');
 });
 
+// Adiciona um ouvinte botão de copiar
 document.getElementById('copiar').addEventListener('click', function () {
+    // obtem a mensagem do elemento de resultado
     const mensagem = document.getElementById('resultado');
+    // Cria uma área de transferência temporária
     const areaTransferencia = document.createElement('textarea');
-    areaTransferencia.value = mensagem.innerText;
+    // define o valor da área de transferência como a mensagem
+    areaTransferencia.value = mensagem.value;
+    // Adiciona a área de transferencia ao corpo do documento
     document.body.appendChild(areaTransferencia);
+    // seleciona o texto na area de transferência
     areaTransferencia.select();
+    // executa o comando de cópia
     document.execCommand('copy');
+    // remove a area de transferência temporaria
     document.body.removeChild(areaTransferencia);
 });
+
+// Função para modificar o Right-Sction
+function esconderMsg() {
+    // esconde a imagem
+    document.querySelector('.img-looking').style.display = 'none';
+    // Mostra o botão de copiar
+    document.getElementById('copiar').style.display = 'block';
+    // esconde o titulo
+    document.querySelector('h1').style.display = 'none';
+    // mostra a caixa de texto com o resultado
+    document.getElementById('resultado').style.display = 'block';
+    // esconde a outra mensagem dessa caixa
+    document.querySelector('h2').style.display = 'none';
+}
